0cb9ec1cb19e3b63e449cd7e5d02332ac96abad3
Prasad Sonawane <72248207+PrasadSonwn@users.noreply.github.com>
2022-02-20 14:42:27 +0530

    Create 709_toLowerCaseEASY.js

---
+++ 709_toLowerCaseEASY.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    
+    if(s.toLowerCase() || s.toUpperCase() || s[0].toLowerCase())      return s.toLowerCase();
+    
+};
+
+/*
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+
+*/
